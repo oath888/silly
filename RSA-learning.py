2810e7cc81cee885f9b85f98291081a0ce198e5f
oath888 <161562415+oath888@users.noreply.github.com>
2025-08-07 08:20:34 +0800

    Update RSA-learning.py

--- RSA-learning.py
+++ RSA-learning.py
@@ -1,22 +1,17 @@
 from cryptography.hazmat.primitives import serialization
 
-with open("C:/id_rsa", "rb") as f:
-    private_key = serialization.load_ssh_private_key(
-        f.read(),
-        password=None,  # 若无密码则填 None
-    )
-# 提取 RSA 参数
-numbers = private_key.private_numbers()
-print(f'p: {numbers.p}\nq:{numbers.q}\nd:{numbers.d}\ndmp1: {numbers.dmp1}\n'
-      f'dmq1: {numbers.dmq1}\niqmp: {numbers.iqmp}\n'
-      f'public_numbers: {numbers.public_numbers}\n')
-
-# from cryptography.hazmat.primitives import serialization
-
-# with open("id_rsa", "rb") as f:
-#     private_key = serialization.load_pem_private_key(  # 改用 PEM 加载函数
-#         f.read(),
-#         password=None,  # 如果密钥未加密
-#     )
-# numbers = private_key.private_numbers()
-# print(numbers.p)
+path = "misc\id_rsa"
+password = None
+try:
+    with open(path,"rb") as f:
+        private_key = serialization.load_pem_private_key(f.read(),password,)
+    numbers = private_key.private_numbers()
+    print(numbers.p, numbers.q, numbers.d, numbers.dmp1, numbers.dmq1, numbers.iqmp)
+except:
+    with open(path,"rb") as f:
+        private_key = serialization.load_ssh_private_key(f.read(),password)
+    numbers = private_key.private_numbers()
+    print(numbers.p, numbers.q, numbers.d, numbers.dmp1, numbers.dmq1, numbers.iqmp)
+    print("This is an SSH key, not a PEM key.")
+else:
+    print("This is a PEM key, not an SSH key.")
